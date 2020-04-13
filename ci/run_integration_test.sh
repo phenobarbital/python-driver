728043d72e0bf2ff39cac5fd19d65a7e8efed15a
Israel Fruchter <fruch@scylladb.com>
2020-04-14 00:02:59 +0300

    build wheel on travis

---
+++ ci/run_integration_test.sh
@@ -0,0 +1,30 @@
+#! /bin/bash -e
+
+# download awscli
+pip install awscli
+
+# install scylla-ccm
+pip install https://github.com/scylladb/scylla-ccm/archive/master.zip
+
+# download version
+LATEST_MASTER_JOB_ID=`aws --no-sign-request s3 ls downloads.scylladb.com/relocatable/unstable/master/ | grep '2020-' | tr -s ' ' | cut -d ' ' -f 3 | tr -d '\/'  | sort -g | tail -n 1`
+AWS_BASE=s3://downloads.scylladb.com/relocatable/unstable/master/${LATEST_MASTER_JOB_ID}
+
+aws s3 --no-sign-request cp ${AWS_BASE}/scylla-package.tar.gz .
+aws s3 --no-sign-request cp ${AWS_BASE}/scylla-tools-package.tar.gz .
+aws s3 --no-sign-request cp ${AWS_BASE}/scylla-jmx-package.tar.gz .
+
+ccm create scylla-driver-temp -n 1 --scylla --version unstable/master:$LATEST_MASTER_JOB_ID \
+ --scylla-core-package-uri=./scylla-package.tar.gz \
+ --scylla-tools-java-package-uri=./scylla-tools-package.tar.gz \
+ --scylla-jmx-package-uri=./scylla-jmx-package.tar.gz
+
+ccm remove
+
+# install driver wheel
+pip install -r test-requirements.txt pytest
+pip install .
+
+# run test
+export SCYLLA_VERSION=unstable/master:$LATEST_MASTER_JOB_ID
+pytest --import-mode append tests/integration/standard/
